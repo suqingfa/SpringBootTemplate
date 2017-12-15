1b376997a3120677650a73a27578f76d65b106b4
苏 <enmoton@qq.com>
2017-12-15 20:42:15 +0800

    Application添加获取Bean和HttpServletRequest静态方法

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,12 +1,19 @@
 package application;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer
+public class Application extends SpringBootServletInitializer implements ApplicationContextAware
 {
     public static void main(String[] args)
     {
@@ -17,5 +24,24 @@ public class Application extends SpringBootServletInitializer
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
     {
         return application.sources(getClass());
+    }
+
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        context = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> requiredType)
+    {
+        return context.getBean(requiredType);
+    }
+
+    public static HttpServletRequest getRequest()
+    {
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        return servletRequestAttributes.getRequest();
     }
 }
