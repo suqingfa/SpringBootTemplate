e92c3d181c52591428bdbac57c8161d7c5df3e87
苏 <enmoton@qq.com>
2017-12-16 18:31:36 +0800

    添加Redis-Session

--- src/main/java/application/config/ApplicationConfig.java
+++ src/main/java/application/config/ApplicationConfig.java
@@ -1,8 +1,10 @@
 package application.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
+@EnableRedisHttpSession
 public class ApplicationConfig
 {
 }
