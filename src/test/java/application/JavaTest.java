7c600b81bbb2d708b238f86126d4483274edf204
苏 <enmoton@qq.com>
2017-12-17 16:53:55 +0800

    testSpEL

--- src/test/java/application/JavaTest.java
+++ src/test/java/application/JavaTest.java
@@ -3,16 +3,27 @@ package application;
 import application.util.GenerateRandomSequence;
 import application.util.GetLogger;
 import org.junit.Test;
+import org.springframework.expression.spel.standard.SpelExpression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 public class JavaTest implements GetLogger
 {
     @Test
     public void test()
     {
-        GenerateRandomSequence randomSequence  = new GenerateRandomSequence();
+        GenerateRandomSequence randomSequence = new GenerateRandomSequence();
         for (int i = 0; i < 100; i++)
         {
             getLogger().debug(randomSequence.getRandomUppercaseNumber());
         }
+    }
+
+    @Test
+    public void testSpEL()
+    {
+        SpelExpressionParser parser = new SpelExpressionParser();
+        SpelExpression expression = parser.parseRaw("'Hello World'.concat('!')");
+        String message = expression.getValue(String.class);
+        getLogger().warn(message);
     }
 }
