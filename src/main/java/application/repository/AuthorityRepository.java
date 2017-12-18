867b6c7169dfbed809c862b9d7c485e9f8778325
苏 <enmoton@qq.com>
2017-12-18 20:22:38 +0800

    Authority添加缓存

--- src/main/java/application/repository/AuthorityRepository.java
+++ src/main/java/application/repository/AuthorityRepository.java
@@ -1,13 +1,20 @@
 package application.repository;
 
 import application.entity.Authority;
+import org.springframework.cache.annotation.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@CacheConfig(cacheNames = "Authority")
 public interface AuthorityRepository extends JpaRepository<Authority, String>
 {
     boolean existsByRole(Authority.Role role);
 
+    @Cacheable
     Authority findByRole(Authority.Role role);
+
+    @CachePut
+    @Override
+    Authority save(Authority authority);
 }
