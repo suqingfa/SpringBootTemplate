47a0f6a8ce7f36b3dbced23b849e420a9d3154f0
苏 <enmoton@qq.com>
2017-12-19 14:30:20 +0800

    修改ModelFromEntityList

--- src/main/java/application/model/ModelFromEntityList.java
+++ src/main/java/application/model/ModelFromEntityList.java
@@ -1,17 +1,16 @@
 package application.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public abstract class ModelFromEntityList<F> extends ModelFromEntity<F>
 {
-    public List<? extends ModelFromEntity<F>> fromEntityList(List<F> fs)
+    public List<ModelFromEntity<F>> fromEntityList(Collection<F> collection)
     {
         Class<? extends ModelFromEntity> tClass = getClass();
         List<ModelFromEntity<F>> output = new ArrayList<>();
         try
         {
-            for (F f : fs)
+            for (F f : collection)
             {
                 output.add(tClass.newInstance().fromEntity(f));
             }
