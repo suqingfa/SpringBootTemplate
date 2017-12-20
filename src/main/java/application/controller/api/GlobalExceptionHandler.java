6c11e571a9d0ac6ce3a74f5c5079750d5731c96f
苏 <enmoton@qq.com>
2017-12-20 14:25:44 +0800

    修改全局异常处理

--- src/main/java/application/controller/api/GlobalExceptionHandler.java
+++ src/main/java/application/controller/api/GlobalExceptionHandler.java
@@ -1,17 +1,20 @@
 package application.controller.api;
 
 import application.model.Output;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import static application.model.Output.outputError;
 
 @ControllerAdvice(basePackageClasses = GlobalExceptionHandler.class)
+@Slf4j
 public class GlobalExceptionHandler
 {
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public Output jsonErrorHandler()
+    public Output jsonErrorHandler(Exception e)
     {
+        log.warn("错误", e);
         return outputError();
     }
 }
