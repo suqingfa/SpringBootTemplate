5991f1eef6555ba8b301df22365585d7681efa49
suqingfa <enmoton@qq.com>
2017-12-31 00:57:07 +0800

    File Entity添加 createTime updateTime字段

--- src/main/java/application/entity/File.java
+++ src/main/java/application/entity/File.java
@@ -1,10 +1,13 @@
 package application.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 @Data
 @Entity
@@ -12,6 +15,14 @@ public class File implements Serializable
 {
     @Id
     private String id;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Timestamp createTime;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Timestamp updateTime;
 
     @Column(nullable = false, columnDefinition = "bytea")
     @Size(min = 10240, max = 10 * 1024 * 1024)
